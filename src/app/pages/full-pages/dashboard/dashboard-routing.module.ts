076346b92022abbef01992168211e129354078e2
Khushang <hushangmirzo@gmail.com>
2020-03-30 13:34:53 +0500

    test commit

--- src/app/pages/full-pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/full-pages/dashboard/dashboard-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard.component';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
   }
 ];
 
